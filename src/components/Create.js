78bce9be731e7b0007e5ba20a6e4e62a8185bbe6
lsh58 <lsh58@gmail.com>
2020-07-07 16:24:17 +0900

    edit 구현

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-function Create({mode, thing, onCreate}){
+function Create({mode, thing, value, onCreate, onCancel, onConfirm, onChange}){
   if(mode==='Create'){
     return (
       <form className="typeArea__wrapper" action="/" onSubmit={onCreate} method="post">
-        <input className="typeArea__create" name="create" placeholder={thing} ></input>
+        <input className="typeArea__create" name="create" placeholder={thing} onChange={onChange} value={value}></input>
         <button type="submit" className="typeArea__addBtn">ADD</button>
       </form>
     );
   }else if(mode==='Edit'){
     return (
-      <form className="typeArea__wrapper" action="/" onSubmit={onCreate} method="post">
-        <input className="typeArea__create" name="create" placeholder={thing} ></input>
-        <button className="typeArea__cancelBtn"></button>
+      <form className="typeArea__wrapper" action="/" onSubmit={onConfirm} method="post">
+        <input className="typeArea__create" name="create" placeholder={thing} onChange={onChange} value={value}></input>
+        <button onClick={onCancel} className="typeArea__cancelBtn"></button>
         <button type="submit" className="typeArea__editBtn">Edit</button>
       </form>
     );

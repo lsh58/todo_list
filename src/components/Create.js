c1403188c4dbf523a4a1c6bbe643035f1d93dc6e
lsh58 <lsh58@gmail.com>
2020-07-07 15:53:26 +0900

    Update Create.js
    mode별로 변경되게끔 수정 (Create,Edit)

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
-function Create({onCreate}){
+function Create({mode, thing, onCreate}){
+  if(mode==='Create'){
     return (
       <form className="typeArea__wrapper" action="/" onSubmit={onCreate} method="post">
-        <input className="typeArea__create" name="create" placeholder="Type what will you do" ></input>
+        <input className="typeArea__create" name="create" placeholder={thing} ></input>
         <button type="submit" className="typeArea__addBtn">ADD</button>
       </form>
     );
+  }else if(mode==='Edit'){
+    return (
+      <form className="typeArea__wrapper" action="/" onSubmit={onCreate} method="post">
+        <input className="typeArea__create" name="create" placeholder={thing} ></input>
+        <button className="typeArea__cancleBtn"></button>
+        <button type="submit" className="typeArea__editBtn">Edit</button>
+      </form>
+    );
+  }
 };
 
 

bf772d7ab585f8535d33bd9f650dcb895c5cf6da
lsh58 <lsh58@gmail.com>
2020-07-07 13:07:14 +0900

    Update TodoList.js
    check시 텍스트에 가로줄긋기 설정

--- src/TodoList.js
+++ src/TodoList.js
@@ -30,11 +30,13 @@ const TodoList = () => {
 
     const onclear = e =>{
         e.preventDefault();
-        if(e.target.classList.contains('list__checkBox__checked')){
+        if(e.target.classList.contains('list__checkBox__checked')){ //체크되어있으면
           e.target.className='list__checkBox'
+          e.target.nextSibling.style.textDecoration="none"
         }
-        else{
+        else{//체크 안되어 있으면
           e.target.className='list__checkBox__checked'
+          e.target.nextSibling.style.textDecoration="line-through"
         }
     }
 
